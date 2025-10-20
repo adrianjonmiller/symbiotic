b6bbcf739750ec3e7e4d8da633cdf21fa986bfe5
Adrian Miller <adrianjonmiller@gmail.com>
2025-10-19 21:17:25 -0500

    Export listen

--- docs/scripts/main.js
+++ docs/scripts/main.js
@@ -2,51 +2,7 @@ import { createSymbiote, bind, repeat } from '../../src/main.js';
 
 const symbiote = createSymbiote({
   '.js-repeat': (el) => {
-    const data = {
-      items: [
-        {
-          id: 1,
-          name: 'Item 1',
-          description: 'Description 1',
-          price: 100,
-          category: 'Category 1',
-          inStock: true,
-          rating: 4.5
-        },
-        {
-          id: 2,
-          name: 'Item 2',
-          description: 'Description 2',
-          price: 200,
-          category: 'Category 2',
-          inStock: false,
-          rating: 3.5
-        },
-        {
-          id: 3,
-          name: 'Item 3',
-          description: 'Description 3',
-          price: 300,
-          category: 'Category 3',
-          inStock: true,
-          rating: 5.0
-        }
-      ]
-    }
-    const updateItems = repeat(el, data)
-
-    setTimeout(() => {
-      data.items.push({
-        id: 4,
-        name: 'Item 4',
-        description: 'Description 4',
-        price: 400,
-        category: 'Category 4',
-        inStock: true,
-        rating: 4.0
-      })
-      updateItems(data)
-    }, 1000)
+    
   }
 });
 
--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ export { destroy, batch } from './mutation.js';
 export { bind, bindAll } from './mutation/bind.js';
 export { createMutator } from './mutation/createMutator.js';
 export { repeat } from './mutation/repeat.js';
+export { listen } from './mutation/listen.js';
 
 // Default export for import createSymbiote from 'symbiotic'
 export { createSymbiote as default, Symbiote } from './Symbiote.js';
